bf1f068d1e0d3984bf1a01e51a880f71aa6959cc
WhiteCatSheriff <henujianye@163.com>
2016-09-24 12:34:33 +0800

    幸运彩票项目第一次提交代码~

---
+++ LuckyLottery/LuckyLottery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LuckyLottery
+//
+//  Created by MS on 16/9/24.
+//  Copyright © 2016年 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LuckyLottery/LuckyLottery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LuckyLottery
+//
+//  Created by MS on 16/9/24.
+//  Copyright © 2016年 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
